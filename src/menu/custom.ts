5df6dcd606bdfda3ef14bcff5cc34f1b0a2ccf76
SicdeX <sicdex@gmail.com>
2025-11-15 23:08:29 +0600

    Refactors custom radius menu and manager
    Improves custom radius node handling by using a fixed
    number of nodes that are hidden instead of being detached
    to avoid performance issues.
    Removes unused options.

--- src/menu/custom.ts
+++ src/menu/custom.ts
@@ -1,18 +1,66 @@
-import { Menu } from "github.com/octarine-public/wrapper/index"
+import {
+	Color,
+	Menu,
+	PARTICLE_RENDER_NAME
+} from "github.com/octarine-public/wrapper/index"
 
 import { EMenuType } from "../enum"
 import { BaseMenu } from "./base"
 
+export class RadiusesMenu {
+	public readonly State: Menu.Toggle
+	public readonly Fill: Menu.Toggle
+	public readonly Radius: Menu.Slider
+	public readonly Style: Menu.Dropdown
+	public readonly Color: Menu.ColorPicker
+	public readonly Node: Menu.Node
+
+	private readonly pNames: PARTICLE_RENDER_NAME[] = [
+		PARTICLE_RENDER_NAME.NORMAL,
+		PARTICLE_RENDER_NAME.ROPE
+	]
+
+	constructor(tree: Menu.Node, idx: number) {
+		this.Node = tree.AddNode(`Radius #${idx}`)
+		this.State = this.Node.AddToggle("State", idx === 1)
+		this.Fill = this.Node.AddToggle("Fill", false, "Fill radius insides color")
+		this.Radius = this.Node.AddSlider("Radius", 1200 * (idx / 1.5), 100, 5000)
+		this.Style = this.Node.AddDropdown("Style", this.pNames)
+		this.Color = this.Node.AddColorPicker("Color", Color.Green)
+	}
+
+	public MenuChanged(cb: () => void) {
+		this.State.OnValue(() => cb())
+		this.Fill.OnValue(() => cb())
+		this.Radius.OnValue(() => cb())
+		this.Style.OnValue(() => cb())
+		this.Color.OnValue(() => cb())
+	}
+}
+
 export class CustomRadiusMenu extends BaseMenu {
+	public readonly Nodes: RadiusesMenu[] = []
 	private static readonly base = "github.com/octarine-public/radiuses/scripts_files/"
 	private static readonly nodeIcon = this.base + "menu/icons/circle-plus.svg"
-
-	public readonly RadiusesCount: Menu.Slider
 
 	constructor(node: Menu.Node) {
 		super(node, EMenuType.Custom, false, CustomRadiusMenu.nodeIcon)
 		this.Tree.SortNodes = false
-		this.Tree.SaveUnusedConfigs = true
-		this.RadiusesCount = this.Tree.AddSlider("Count", 0, 0, 10)
+		for (let i = 1; i < 6; i++) {
+			this.Nodes.push(new RadiusesMenu(this.Tree, i))
+		}
+		Menu.Localization.AddLocalizationUnit("russian", this.GetLocalization)
+	}
+	public MenuChanged(cb: () => void) {
+		for (let i = this.Nodes.length - 1; i > -1; i--) {
+			this.Nodes[i].MenuChanged(cb)
+		}
+	}
+	protected get GetLocalization(): Map<string, string> {
+		const map = new Map<string, string>()
+		for (let i = 1; i < 6; i++) {
+			map.set(`Radius #${i}`, `Радиус #${i}`)
+		}
+		return map
 	}
 }
--- src/modules/custom.ts
+++ src/modules/custom.ts
@@ -1,58 +1,31 @@
 import {
-	Color,
 	DOTAGameState,
 	Hero,
 	LocalPlayer,
-	Menu,
-	PARTICLE_RENDER_NAME,
 	ParticleAttachment,
 	ParticlesSDK
 } from "github.com/octarine-public/wrapper/index"
 
+import { RadiusesMenu } from "../menu/custom"
 import { MenuManager } from "../menu/index"
 import { BaseManager } from "./base"
 
-interface CustomRadiusNode {
-	readonly range: Menu.Slider
-	readonly node: Menu.IMenuParticlePicker
-}
-
 export class CustomRadiusManager extends BaseManager {
-	private readonly radiusesNodes: CustomRadiusNode[] = []
-	private readonly pNames: PARTICLE_RENDER_NAME[] = [
-		PARTICLE_RENDER_NAME.NORMAL,
-		PARTICLE_RENDER_NAME.ROPE
-	]
-
 	constructor(
 		protected readonly menu: MenuManager,
 		protected readonly pSDK: ParticlesSDK
 	) {
 		super(menu, pSDK)
-		this.RadiusesCount.OnValue(call => this.AddOrRemoveNode(call.value))
-		Menu.Localization.AddLocalizationUnit("russian", this.GetLocalization)
+		this.menu.CustomRadiusMenu.MenuChanged(this.MenuChanged.bind(this))
 	}
 
 	protected get RadiusMenu() {
 		return this.menu.CustomRadiusMenu
 	}
 
-	protected get RadiusesCount() {
-		return this.RadiusMenu.RadiusesCount
-	}
-
 	protected get State() {
 		return this.menu.State.value && this.RadiusMenu.State.value
 	}
-
-	protected get GetLocalization(): Map<string, string> {
-		const map = new Map<string, string>()
-		for (let i = 0; i < this.RadiusesCount.max; i++) {
-			map.set(`Radius #${i + 1}`, `Радиус #${i + 1}`)
-		}
-		return map
-	}
-
 	public GameStateChanged(newState: DOTAGameState) {
 		switch (newState) {
 			case DOTAGameState.DOTA_GAMERULES_STATE_PRE_GAME:
@@ -65,90 +38,44 @@ export class CustomRadiusManager extends BaseManager {
 				break
 		}
 	}
-
 	public MenuChanged() {
 		this.UpdateRadiusByArr()
 	}
-
 	public EntityCreated(entity: Hero) {
 		if (entity.IsMyHero) {
 			this.UpdateRadiusByArr()
 		}
 	}
-
 	public EntityDestroyed(entity: Hero) {
 		if (entity.IsMyHero) {
 			this.UpdateRadiusByArr(true)
 		}
 	}
-
 	public LifeStateChanged(entity: Hero) {
 		if (entity.IsMyHero) {
 			this.UpdateRadiusByArr(!entity.IsAlive)
 		}
 	}
-
-	public GameStarted() {
-		this.UpdateRadiusByArr()
-	}
-
 	public GameEnded() {
-		this.UpdateRadiusByArr(true)
+		this.pSDK.DestroyAll()
 	}
-
-	protected AddOrRemoveNode(currValue: number) {
-		const totalCount = this.radiusesNodes.length
-		if (totalCount < currValue) {
-			this.AddNode(currValue, totalCount)
-			this.RadiusMenu.Tree.Update()
-			return
-		}
-		const nodes = this.radiusesNodes.splice(currValue, totalCount - currValue)
-		for (let index = 0, end = nodes.length; index < end; index++) {
-			const nodeObj = nodes[index]
-			nodeObj.node.Node.DetachFromParent()
-			this.UpdateRadius(nodeObj, true)
-			this.RadiusMenu.Tree.Update()
-		}
-	}
-
-	protected AddNode(currValue: number, totalCount: number) {
-		for (let index = 0; index < currValue - totalCount; index++) {
-			const visibleID = totalCount + index + 1
-			const node = this.RadiusMenu.Tree.AddParticlePicker(
-				`Radius #${visibleID}`,
-				Color.Green.SetA(255 / 2),
-				this.pNames,
-				[true, true]
-			)
-			node.Fill.OnValue(() => this.UpdateRadiusByArr())
-			node.Color.OnValue(() => this.UpdateRadiusByArr())
-			node.Style.OnValue(() => this.UpdateRadiusByArr())
-			node.State?.OnValue(() => this.UpdateRadiusByArr())
-			const range = node.Node.AddSlider("Radius", 1200 * (visibleID / 1.5), 1, 5000)
-			range.OnValue(() => this.UpdateRadiusByArr())
-			this.radiusesNodes.push({ node, range })
-		}
-	}
-
-	protected UpdateRadius(obj: CustomRadiusNode, destroy = false) {
+	protected UpdateRadius(obj: RadiusesMenu, destroy = false) {
 		const localHero = LocalPlayer?.Hero
-		const keyName = obj.node.Node.InternalName.split(" ").join("_")
-		if (destroy || localHero === undefined || !this.State || !obj.node.State?.value) {
+		const keyName = obj.Node.InternalName.split(" ").join("_")
+		if (destroy || localHero === undefined || !this.State || !obj.State.value) {
 			this.pSDK.DestroyByKey(keyName)
 			return
 		}
-		this.pSDK.DrawCircle(keyName, localHero, obj.range.value, {
-			Color: obj.node.Color.SelectedColor,
-			Fill: obj.node.Fill.value,
-			RenderStyle: obj.node.Style.SelectedID,
+		this.pSDK.DrawCircle(keyName, localHero, obj.Radius.value, {
+			Color: obj.Color.SelectedColor,
+			Fill: obj.Fill.value,
+			RenderStyle: obj.Style.SelectedID,
 			Attachment: ParticleAttachment.PATTACH_ABSORIGIN_FOLLOW
 		})
 	}
-
 	protected UpdateRadiusByArr(destroy = false) {
-		for (let index = this.radiusesNodes.length - 1; index > -1; index--) {
-			this.UpdateRadius(this.radiusesNodes[index], destroy)
+		for (let i = this.RadiusMenu.Nodes.length - 1; i > -1; i--) {
+			this.UpdateRadius(this.RadiusMenu.Nodes[i], destroy)
 		}
 	}
 }
